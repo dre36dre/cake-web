12881910c441a3302d0fbd82c31d1aa2fe4d4068
Anderson freires <dre.son36@gmail.com>
2026-03-06 12:07:38 -0300

    Inicio Angular frontend cake

---
+++ src/app/features/features/cake/cake-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CakeRoutingModule } from './cake-routing-module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, CakeRoutingModule],
+})
+export class CakeModule {}
