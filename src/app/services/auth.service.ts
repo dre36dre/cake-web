b6e37dc5c1bb6eea22bdc19d4b7614f6e1126f09
Anderson freires <dre.son36@gmail.com>
2026-03-09 20:44:28 -0300

    Continuar a partir de Criar o HTML da tela de login

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,18 +1,31 @@
-import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { LoginRequest } from '../models/login-request';
+import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  private apiUrl = 'http://localhost:8080/api/auth';
+  private api = 'http://localhost:8080/api/auth';
 
   constructor(private http: HttpClient) {}
 
-  login(data: LoginRequest): Observable<any> {
-    return this.http.post(`${this.apiUrl}/login`, data);
+  login(email: string, password: string){
+    return this.http.post<any>(`${this.api}/login`, {
+      email,
+      password
+    });
+  }
+
+  saveToken(token: string){
+    localStorage.setItem('token', token);
+  }
+
+  getToken(){
+    return localStorage.getItem('token');
+  }
+
+  logout(){
+    localStorage.removeItem('token');
   }
 }
