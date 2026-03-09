bd6567f40d3cd588d97add0e02aa7aca73f89abc
Anderson freires <dre.son36@gmail.com>
2026-03-09 17:07:35 -0300

    Continuar desde o inicio em criar modulo de autenticação

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { LoginRequest } from '../models/login-request';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private apiUrl = 'http://localhost:8080/api/auth';
+
+  constructor(private http: HttpClient) {}
+
+  login(data: LoginRequest): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, data);
+  }
+}
